a1d2b834248002c8b2e26f184cc810392273afe6
YuldoshMakhmud <154692684+YuldoshMakhmud@users.noreply.github.com>
2024-06-15 19:55:00 +0900

    quizapp

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,3 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:quiz/questions_screen.dart';
+import 'package:quiz/start_screen.dart';
 
-class Quiz extends StatefulWidget
+
+class Quiz extends StatefulWidget {
+
+  const Quiz({super.key});
+  @override
+  State<Quiz> createState() {
+    return _QuizState();
+  }
+}
+
+class _QuizState extends State<Quiz>{
+  Widget activeScreen = const StartScreen();
+
+  void switchScreeen(){
+    setState(() {
+      activeScreen = const QuestionsScreen();
+    });
+  }
+  @override
+  Widget build(context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Color.fromARGB(255, 78, 13, 151),
+                  Color.fromARGB(255, 107, 15, 168)
+                ],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              )
+          ),
+          child: activeScreen,
+        ),
+      ),
+    );
+  }
+}
