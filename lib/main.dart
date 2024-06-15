2cc553c48bc932f9def324ea1323a5ca6a056f09
YuldoshMakhmud <154692684+YuldoshMakhmud@users.noreply.github.com>
2024-06-15 13:18:32 +0900

    quizapp

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:quiz/start_screen.dart';
 
 void main() {
-  runApp(MaterialApp(
+  runApp( MaterialApp(
     home: Scaffold(
-      body: StartScreen(),
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color.fromARGB(255, 78, 13, 151),
+              Color.fromARGB(255, 107, 15, 168)
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          )
+        ),
+        child: const  StartScreen(),
+      ),
     ),
   ));
 }
--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -1,11 +1,37 @@
 import 'package:flutter/material.dart';
 
 class StartScreen extends StatelessWidget {
-  const StartScreen ({super.key});
+  const StartScreen({super.key});
 
   @override
-  Widget build(context){
-    return Text("Start Screen");
+  Widget build(context) {
+    return Center(
+        child: Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Image.asset(
+          'assets/images/photo_2024-06-15_12-19-34.jpg.png',
+          width: 400,
+          color: Colors.white,
+        ),
+        const SizedBox(
+          height: 70,
+        ),
+        const Text(
+          "Learn Reading the fun way",
+          style: TextStyle(
+              color: Color.fromARGB(255, 237, 223, 252), fontSize: 24),
+        ),
+        const SizedBox(
+          height: 30,
+        ),
+        OutlinedButton.icon(
+          onPressed: () {},
+          style: OutlinedButton.styleFrom(foregroundColor: Colors.white),
+          icon: const Icon(Icons.arrow_right_alt),
+          label: const Text("Start Quiz"),
+        )
+      ],
+    ));
   }
-
 }
