ac49a0e133c3235072836bddb31573250fa2474d
YuldoshMakhmud <154692684+YuldoshMakhmud@users.noreply.github.com>
2024-06-16 18:56:06 +0900

    quizapp

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -13,21 +13,20 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz>{
-  Widget? activeScreen;
+  var activeScreen = 'start-screen';
 
-  @override
-  void initState(){
-    activeScreen = StartScreen(switchScreen);
-    super.initState();
-  }
 
   void switchScreen(){
     setState(() {
-      activeScreen = const QuestionsScreen();
+      activeScreen ='questions-screen';
     });
   }
   @override
   Widget build(context) {
+   Widget screenWidget = StartScreen(switchScreen);
+   if(activeScreen == 'questions-screen'){
+     screenWidget = const QuestionsScreen();
+   }
     return MaterialApp(
       home: Scaffold(
         body: Container(
@@ -41,7 +40,7 @@ class _QuizState extends State<Quiz>{
                 end: Alignment.bottomRight,
               )
           ),
-          child: activeScreen,
+          child: screenWidget,
         ),
       ),
     );
