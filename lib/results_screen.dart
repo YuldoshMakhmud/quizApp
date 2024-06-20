62a8edd0d135c597fd2c110970a1c7d3aa5a8b8c
YuldoshMakhmud <154692684+YuldoshMakhmud@users.noreply.github.com>
2024-06-20 10:09:38 +0900

    quizapp

--- lib/results_screen.dart
+++ lib/results_screen.dart
@@ -1,10 +1,26 @@
 import 'package:flutter/material.dart';
-
+import 'package:quiz/data/questions.dart';
+import 'package:quiz/questions_summary.dart';
 class ResultsScreen extends StatelessWidget {
-  const ResultsScreen({super.key,required this.chosenAnswer});
+  const ResultsScreen({
+    super.key,
+    required this.chosenAnswer});
 
   final List<String> chosenAnswer;
 
+  List<Map<String,Object>> getSummaryData() {
+    final List<Map<String, Object>> summary = [];
+
+    for (var i = 0; i < chosenAnswer.length; i++) {
+      summary.add({
+        'question_index': i,
+        'question': questions[i].text,
+        'correct_answer': questions[i].answers[0],
+        'user_answer': chosenAnswer[i]
+      });
+    }
+    return summary;
+  }
   @override
   Widget build(BuildContext context) {
     return SizedBox(
@@ -16,7 +32,7 @@ class ResultsScreen extends StatelessWidget {
             children: [
              const Text('you ansverd X out of Y questions correctly!'),
              const  SizedBox(height: 30,),
-             const Text('List of answers and questions ...'),
+             QuestionsSummary(getSummaryData()),
              const SizedBox(height: 30,),
               TextButton(onPressed: (){} , child: const Text('Restart quiz')
               ),
